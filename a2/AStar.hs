f73cf91410302ae48065ed3b66fc84e235cfbc2b
nelk <nelkishere@gmail.com>
2015-01-29 21:51:07 -0500

    Documented A* stuff.

--- a2/AStar.hs
+++ a2/AStar.hs
@@ -5,63 +5,114 @@ import qualified Search
 import qualified Data.Set as Set
 import qualified Data.Heap as Heap
 
-data (Search.ProblemNode pn k, Show pn) => AStarNode pn k = AStarNode (Search.Path pn) pn
+-- |A generic search node for A*. It holds the path through problem nodes
+-- searched so far and the current problem node.
+data (Search.ProblemNode pn k, Show pn)
+  => AStarNode pn k = AStarNode (Search.Path pn) pn
   deriving Show
 
+-- |A type holding the problem information required for A*.
 data Search.ProblemNode pn k => ProblemDef pn k = ProblemDef
-  { successors :: pn -> [pn]
+  { -- A function to generate the neighbours of a given problem node.
+    successors :: pn -> [pn]
+    -- The cost of the current search path.
   , costSoFar :: Search.Path pn -> Search.Cost
+    -- An admissable heuristic function that estimates the cost from
+    -- a given problem node to the goal node.
   , heuristicCostToEnd :: pn -> Search.Cost
+    -- Whether a given problem node is a goal.
   , isGoalNode :: pn -> Bool
   }
 
+-- |Making AStarState a searcher that can be used by the generic search
+-- algorithm.
+instance (Search.ProblemNode pn k, Show pn)
+      => Search.Searcher (AStarState pn k) pn where
+  -- The next node is just the top of the heap.
+  nextNode = fmap (\(_, AStarNode _ n) -> n) . Heap.viewHead . fringe
+
+  -- The search path through problem nodes is also stored in the A* node
+  -- at the top of the heap.
+  searchPath s = case Heap.viewHead (fringe s) of
+                Nothing -> []
+                Just (_, AStarNode path _) -> path
+
+  -- We just ask the problem definition if the next node is a goal node.
+  isAtGoal s = case Search.nextNode s of
+                    Nothing -> False
+                    Just pn -> isGoalNode (problemDef s) pn
+
+  -- Here is where the work happens to create a new A* state by expanding a
+  -- node.
+  expandNextNode s = case Heap.view $ fringe s of
+    Nothing -> s
+    -- Remove the top of the fringe heap.
+    Just ((_, AStarNode curPath curProblemNode), newFringe) ->
+      let prob = problemDef s
+          -- Generate neighbours of the current node.
+          nextProblemNodes = successors prob curProblemNode
+          -- Filter any neighbours that have been processed already.
+          nextUnprocessedProblemNodes =
+            filter (not . flip Set.member (processed s) . Search.ident)
+                   nextProblemNodes
+          -- Turn the successor problem nodes into A* nodes by adding them to
+          -- the search path gone so far to reach the current node.
+          nextNodes = map (\probNode -> AStarNode (probNode:curPath) probNode)
+                          nextUnprocessedProblemNodes
+          -- Add all of these new states to the heap.
+          newFringe' = foldl (\h n' -> Heap.insert (nodeCost prob n', n') h)
+                             newFringe nextNodes
+          -- Add the current node to the set of processed nodes.
+          newProcessed = Set.insert (Search.ident curProblemNode) $ processed s
+
+      -- Build the next searcher state from the data calculated above.
+      in s { fringe = newFringe'
+           , processed = newProcessed
+           , successorCount = successorCount s + length nextUnprocessedProblemNodes
+           , processedCount = processedCount s + 1
+           }
+
+-- |A* searcher state.
 data AStarState pn k = AStarState
-  { fringe :: !(Heap.MinPrioHeap Search.Cost (AStarNode pn k))
+  { -- A min heap to process successors with lowest cost + heuristic cost first.
+    fringe :: !(Heap.MinPrioHeap Search.Cost (AStarNode pn k))
+    -- A set to keep track of what problem nodes have been seen already.
   , processed :: !(Set.Set k)
+    -- The problem info given by the user.
   , problemDef :: ProblemDef pn k
+    -- Two counters for measuring how A* performed.
   , successorCount :: !Int
   , processedCount :: !Int
   }
 
 -- Note: Returns path in reverse order.
-aStarSearch :: (Search.ProblemNode pn k, Show pn) => ProblemDef pn k -> pn -> (Maybe (Search.Path pn), Int, Int)
-aStarSearch probDef startProblemNode = (fst soln, processedCount $ snd soln, successorCount $ snd soln)
-  where searcher = AStarState { problemDef = probDef
-                              , processed = Set.empty
-                              , fringe = Heap.singleton (nodeCost probDef startAStarNode, startAStarNode)
-                              , successorCount = 0
-                              , processedCount = 0
-                              }
+-- |Solve a given problem using A*. It takes a problem and a starting problem
+-- node, and tries to return a solution along with performance stats.
+aStarSearch :: (Search.ProblemNode pn k, Show pn)
+            => ProblemDef pn k -> pn -> (Maybe (Search.Path pn), Int, Int)
+aStarSearch probDef startProblemNode =
+  -- Use solution from generic search and output with counter values.
+  (fst soln, processedCount $ snd soln, successorCount $ snd soln)
+  where -- Construct the initial A* state.
+        searcher = AStarState
+          { problemDef = probDef
+          , processed = Set.empty
+          , fringe = Heap.singleton
+                       (nodeCost probDef startAStarNode, startAStarNode)
+          , successorCount = 0
+          , processedCount = 0
+          }
+        -- Wrap starting problem node in A* node.
         startAStarNode = AStarNode [startProblemNode] startProblemNode
+        -- Get solution using generic search method.
         soln = Search.search searcher
 
-nodeCost :: (Search.ProblemNode pn k, Show pn) => ProblemDef pn k -> AStarNode pn k -> Search.Cost
-nodeCost prob (AStarNode path cur) = costSoFar prob path + heuristicCostToEnd prob cur
+-- |The cost function for A* which adds the known cost of the search path so
+-- far and the heuristic cost from the current problem node to the goal.
+nodeCost :: (Search.ProblemNode pn k, Show pn)
+         => ProblemDef pn k -> AStarNode pn k -> Search.Cost
+nodeCost prob (AStarNode path cur) = costSoFar prob path
+                                   + heuristicCostToEnd prob cur
 
-instance (Search.ProblemNode pn k, Show pn) => Search.Searcher (AStarState pn k) pn where
-  nextNode = fmap (\(_, AStarNode _ n) -> n) . Heap.viewHead . fringe
-
-  searchPath s = case Heap.viewHead (fringe s) of
-                Nothing -> []
-                Just (_, AStarNode path _) -> path
-
-  isAtGoal s = case Search.nextNode s of
-                    Nothing -> False
-                    Just pn -> isGoalNode (problemDef s) pn
-
-  expandNextNode s = case Heap.view $ fringe s of
-                      Nothing -> s
-                      Just ((_, AStarNode curPath curProblemNode), newFringe) ->
-                        let prob = problemDef s
-                            nextProblemNodes = successors prob curProblemNode
-                            nextUnprocessedProblemNodes = filter (not . flip Set.member (processed s) . Search.ident) nextProblemNodes
-                            nextNodes = map (\probNode -> AStarNode (probNode:curPath) probNode) nextUnprocessedProblemNodes
-                            newFringe' = foldl (\h n' -> Heap.insert (nodeCost prob n', n') h) newFringe nextNodes
-                            newProcessed = Set.insert (Search.ident curProblemNode) $ processed s
-                        in s { fringe = newFringe'
-                             , processed = newProcessed
-                             , successorCount = successorCount s + length nextUnprocessedProblemNodes
-                             , processedCount = processedCount s + 1
-                             }
 
 
--- a2/TSPAStar.hs
+++ a2/TSPAStar.hs
@@ -10,53 +10,101 @@ import qualified Search
 import TSP
 import qualified AStar
 
+-- |Use A* to solve a TSP with a given heuristic function.
 solveWithAStar :: (Tour -> Search.Cost) -> TSP -> (Maybe (Search.Path Tour), Int, Int)
 solveWithAStar heuristic tsp =
   let problem = mkAStarTSPProblem heuristic tsp
   in AStar.aStarSearch problem (Tour [startCity])
 
+-- |A simple heuristic function that simply counts how many cities are left
+-- to visit.
 heuristicNumRemainingCities :: TSP -> Tour -> Search.Cost
-heuristicNumRemainingCities tsp (Tour tour) = fromIntegral $ length (tspCities tsp) - length tour
+heuristicNumRemainingCities tsp (Tour tour) =
+  fromIntegral $ length (tspCities tsp) - length tour
 
+-- |A simple heuristic that finds the maximum of
+-- (dist(current,c) + dist(c, first_city)) over all unvisited cities c.
 heuristicFarthestSingleCityThenGoal :: TSP -> Tour -> Search.Cost
 heuristicFarthestSingleCityThenGoal tsp (Tour tour) =
   let leftToVisit = notYetVisited tsp tour
       curCity = head tour
       goalCity = last tour
-      toFarthestThenGoalCost = maximum $ map (\city -> cityDist tsp curCity city + cityDist tsp city goalCity) leftToVisit
+      toFarthestThenGoalCost =
+        maximum $ map (\city ->
+          cityDist tsp curCity city + cityDist tsp city goalCity)
+                      leftToVisit
   in if null leftToVisit
        then 0
        else toFarthestThenGoalCost
 
-type Edge = (City, City, Search.Cost)
+-- |Representations for the MST heuristic.
+-- MST is computed using a heap to add edges from shortest to longest, and
+-- a UnionFind data structure to keep track of which nodes are connected.
+
+-- An edge in the MST is represented by its two city endpoints and their
+-- distance. The cities are using a UnionFind type so that they can be
+-- looked up directly.
 type PointEdge = (UnionFind.Point City, UnionFind.Point City, Search.Cost)
 
-type UF = UnionFind.UnionFindT City (State (Heap.MinPrioHeap Search.Cost PointEdge))
+-- |This is an alias for the UnionFind Monad Transformer with the identifier
+-- and inner State types we need.
+type UF = UnionFind.UnionFindT City
+          (State (Heap.MinPrioHeap Search.Cost PointEdge))
 
+-- |Initialize the heap used for the MST algorithm below from a list of cities.
 initMST_ :: [City] -> (City -> City -> Search.Cost) -> UF ()
 initMST_ cities cost = do
+  -- Create UnionFind identifiers for all cities.
   points <- mapM UnionFind.fresh cities
+  -- Create an edge for the MST algorithm for every pair of cities.
   let edges = map (uncurry mkEdge) $ pairs $ zip cities points
+  -- Put all of these edges in the heap with priority equal to the edge length.
   lift $ put $ Heap.fromList $ map (\e@(_, _, c) -> (c, e)) edges
 
- where mkEdge :: (City, UnionFind.Point City) -> (City, UnionFind.Point City) -> PointEdge
+ where -- Helper function to create a PointEdge out of two cities and their
+       -- UnionFind representations.
+       mkEdge :: (City, UnionFind.Point City)
+              -> (City, UnionFind.Point City)
+              -> PointEdge
        mkEdge (a, pa) (b, pb) = (pa, pb, cost a b)
+
+       -- Helper function that returns all pairs of items given a list of items.
        pairs :: [a] -> [(a, a)]
+       -- An empty list has no pairs.
        pairs [] = []
+       -- Take the first item in the list as the first item in a pair, and
+       -- make a pair with every other item in the list. Then append
+       -- all pairs of the remainder of the list by recursing on it.
        pairs (n:ns) = ((,) n <$> ns) ++ pairs ns
 
+-- |Implementation of finding the MST cost.
 findMST_ :: UF Search.Cost
 findMST_ = do
+  -- Get the internal heap state.
   (heap :: Heap.MinPrioHeap Search.Cost PointEdge) <- lift get
   case Heap.view heap of
+    -- If no edges left in heap, return 0 cost.
     Nothing -> return 0.0
+    -- Take top item off of heap.
     Just ((_, (a, b, c)), heap') -> do
+      -- Set internal heap to be heap with top element removed.
       lift $ put heap'
+      -- Check if the two cities of this edge are in the same UnionFind set.
+      -- If they are then they are already connected and so adding this
+      -- edge would cause a cycle in the spanning tree.
       isCycle <- UnionFind.equivalent a b
+      -- Union the two cities
+      -- (will have no effect if they are already connected).
       UnionFind.union a b
+      -- Recurse with the current heap and UnionFind state to get the cost
+      -- of the rest of the edges in the MST.
       rest <- findMST_
+      -- Return the total cost. Include the edge taken above unless it would
+      -- form a cycle.
       return $ if isCycle then rest else c + rest
 
+-- |Find the cost of an MST given a list of cities and a cost function that
+-- returns the length of edge between two cities.
 findMST :: [City] -> (City -> City -> Search.Cost) -> Search.Cost
 findMST cities cost =
   let st :: State (Heap.MinPrioHeap Search.Cost PointEdge) Search.Cost
@@ -64,25 +112,41 @@ findMST cities cost =
       (mst, _) = runState st Heap.empty
   in mst
 
+-- |A good heuristic function that computes the cost of an MST of the unvisited
+-- nodes and adds the shortest edges connecting the current node to it and
+-- the starting state to it.
 heuristicMST :: TSP -> Tour -> Search.Cost
 heuristicMST tsp (Tour tour) =
-  let nodes = notYetVisited tsp tour
+  let -- The nodes of the MST will be unvisited cities.
+      nodes = notYetVisited tsp tour
+      -- Compute the MST cost using the above algorithm.
       mstCost = findMST nodes (cityDist tsp)
       cur = head tour
-      goal = startCity -- Could make more general by taking last tour.
+      goal = startCity
+      -- Shortest edge connecting current city to MST.
       curToClosest = minimum $ map (cityDist tsp cur) nodes
+      -- Shortest edge connecting first city to MST.
       goalToClosest = minimum $ map (cityDist tsp goal) nodes
+      -- Heuristic cost. Zero if no unvisited cities, otherwise sum
+      -- the MST cost and the two edges found above.
       h | null nodes = 0.0
         | otherwise = mstCost + curToClosest + goalToClosest
   in h
 
 -- For this problem, ProblemNodes are actually tours.
-mkAStarTSPProblem :: (Tour -> Search.Cost) -> TSP -> AStar.ProblemDef Tour [City]
+-- |Create the problem definition for a TSP for A* given a heuristic function.
+mkAStarTSPProblem :: (Tour -> Search.Cost)
+                  -> TSP
+                  -> AStar.ProblemDef Tour [City]
 mkAStarTSPProblem heuristic tsp = AStar.ProblemDef
   { AStar.isGoalNode = isGoalNode
   , AStar.successors = successors
+    -- Cost only depends on the problem node (partial tour), not the A* search
+    -- nodes that have been traversed so far.
   , AStar.costSoFar = \(tour:_) -> tourCost tsp tour
   , AStar.heuristicCostToEnd = heuristic
   }
-  where isGoalNode (Tour tour) = length tour == length (tspCities tsp)
+  where -- A goal node is just a complete tour (no repeating start node).
+        isGoalNode (Tour tour) = length tour == length (tspCities tsp)
+        -- A successor is created for visiting each unvisited city next.
         successors (Tour tour) = map (Tour . (:tour)) $ notYetVisited tsp tour
