8f0e7e1ad5482636e63b0e2242c0e6662174a973
nelk <nelkishere@gmail.com>
2015-02-09 14:58:40 -0500

    Added sudoku variable and constraint creation.

--- a3/SudokuSolver.hs
+++ a3/SudokuSolver.hs
@@ -2,9 +2,12 @@
 module SudokuSolver where
 
 import qualified Data.Set as Set
+import Data.List (sortBy)
+import Data.Ord (comparing)
+import Control.Applicative
 import ConstraintSolver
 
-data Digits = D1 | D2 | D3 | D4 | D5 | D6 | D7 | D8 | D9
+data Digit = D1 | D2 | D3 | D4 | D5 | D6 | D7 | D8 | D9
   deriving (Show, Eq, Ord, Enum)
 data Row = R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9
   deriving (Show, Eq, Ord, Enum)
@@ -12,10 +15,10 @@ data Col = C1 | C2 | C3 | C4 | C5 | C6 | C7 | C8 | C9
   deriving (Show, Eq, Ord, Enum)
 type Cell = (Row, Col)
 
-type SudokuVar = Var Cell Digits
+type SudokuVar = Var Cell Digit
 
 data SudokuConstraint = Alldiff (Set.Set Cell)
-instance Constraint SudokuConstraint Cell Digits where
+instance Constraint SudokuConstraint Cell Digit where
   included (Alldiff cellSet) var
     = varId var `Set.member` cellSet
 
@@ -36,9 +39,47 @@ instance Constraint SudokuConstraint Cell Digits where
 
   restrict _ (UnassignedVar _ _) vs = Just vs
 
-sudoku :: ConstraintProblem SudokuConstraint Cell Digits
-sudoku = ConstraintProblem
-  { initialVars = []
-  , constraints = []
+zipMatching :: Ord i => (a -> i) -> [a] -> (b -> i) -> [b] -> [(Maybe a, Maybe b)]
+zipMatching _      []     _      []     = []
+zipMatching identA []     identB (b:bs) = (Nothing, Just b):zipMatching identA [] identB bs
+zipMatching identA (a:as) identB []     = (Just a, Nothing):zipMatching identA as identB []
+zipMatching identA (a:as) identB (b:bs) = case compare (identA a) (identB b) of
+  EQ -> (Just a, Just b):zipMatching identA as identB bs
+  LT -> (Just a, Nothing):zipMatching identA as identB (b:bs)
+  GT -> (Nothing, Just b):zipMatching identA (a:as) identB bs
+
+makeVarsFromStartingState :: [(Cell, Digit)] -> [Var Cell Digit]
+makeVarsFromStartingState startVals =
+  let sortedStartVals = sortBy (comparing fst) startVals
+      allCells = [(i, j) | i <- enumFromTo R1 R9, j <- enumFromTo C1 C9]
+      zippedStarting = zipMatching fst sortedStartVals id allCells
+      makeVar (_, Nothing) = error "Got unknown cell as starting Val."
+      makeVar (Nothing, Just cell) = UnassignedVar cell $ Set.fromList $ enumFromTo D1 D9
+      makeVar (Just (_, v), Just cell) = AssignedVar cell v
+  in map makeVar zippedStarting
+
+sudokuConstraints :: [SudokuConstraint]
+sudokuConstraints =
+  -- Each number in the same row is different.
+  map
+    (\i -> Alldiff $ Set.fromList [(i, j) | j <- enumFromTo C1 C9])
+    (enumFromTo R1 R9)
+  ++
+  -- Each number in the same column is different.
+  map
+    (\j -> Alldiff $ Set.fromList [(i, j) | i <- enumFromTo R1 R9])
+    (enumFromTo C1 C9)
+  ++
+  -- Each number in the same square is different.
+  map
+    (\(rowOff, colOff) -> Alldiff $ Set.fromList [(i, j) | i <- enumFromTo rowOff (succ $ succ rowOff), j <- enumFromTo colOff (succ $ succ colOff)])
+    ((,) <$> [R1, R4, R7] <*> [C1, C4, C7])
+
+
+
+sudoku :: [(Cell, Digit)] -> ConstraintProblem SudokuConstraint Cell Digit
+sudoku startVals = ConstraintProblem
+  { initialVars = makeVarsFromStartingState startVals
+  , constraints = sudokuConstraints
   }
 
