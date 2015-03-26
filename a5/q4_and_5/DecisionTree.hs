4d3ed49f1b6ee642f6b94ada8af2ee40425058d9
nelk <nelkishere@gmail.com>
2015-03-26 01:46:09 -0400

    A5 Q4 code documentation.

--- a5/q4_and_5/DecisionTree.hs
+++ a5/q4_and_5/DecisionTree.hs
@@ -6,18 +6,24 @@ import Data.Ord (comparing)
 import Control.Monad.State
 import Control.Monad.Writer
 
+-- |Types for this domain.
 type Attribute = Int
 type FeatureName = String
 type Clss = String
+type Threshold = Float
 data Example = Example
-  { exAttrs :: Vector Float
+  { exAttrs :: Vector Threshold
   , exClass :: Clss
   }
   deriving (Show, Eq)
 
+-- |Decision Tree data structure.
+-- Either a leaf, or a branch that divides on an attribute and threshold.
+-- The left child represents values less than or equal to the threshold.
 data DecisionTree = Leaf Clss
-                  | Branch Attribute Float DecisionTree DecisionTree
+                  | Branch Attribute Threshold DecisionTree DecisionTree
 
+-- |Are all elements in this list equal?
 allEqual :: Eq a => [a] -> Bool
 allEqual [] = True
 allEqual [_] = True
@@ -30,34 +36,46 @@ log2 = logBase 2
 lengthF :: [a] -> Float
 lengthF = fromIntegral . length
 
+-- |Partition examples by their classification.
 partitionByClass :: [Example] -> ([Example], [Example])
 partitionByClass examples =
   let a_class = exClass $ head examples
   in partition ((== a_class) . exClass) examples
 
-partitionByAttr :: Attribute -> Float -> [Example] -> ([Example], [Example])
+-- |Partition examples by an attribute and threshold.
+partitionByAttr :: Attribute -> Threshold -> [Example] -> ([Example], [Example])
 partitionByAttr attr thresh =
   partition (\e -> exAttrs e ! attr <= thresh)
 
+-- |Boolean entropy formula for a probability q.
 boolEntropy :: Float -> Float
 boolEntropy q = -(q * log2 q + (1 - q) * log2 (1 - q))
 
+-- |Entropy of a group of examples.
 entropy :: [Example] -> Float
 entropy examples =
+  -- Partition them by their classification.
   let (positives, negatives) = partitionByClass examples
+      -- Special case - if all are positive or all are negative, entropy is 0.
       entr | null positives || null negatives = 0.0
+      -- Otherwise it's the boolean entropy of the fraction with are positive or negative.
            | otherwise = boolEntropy $ lengthF positives / lengthF examples
   in entr
 
-remainder :: [Example] -> Attribute -> Float -> Float
+-- |Remaining entropy after splitting given examples by given attribute and threshold.
+remainder :: [Example] -> Attribute -> Threshold -> Float
 remainder examples attr thresh =
+  -- Partition by the attribute and threshold.
   let (lefts, rights) = partitionByAttr attr thresh examples
+  -- Sum of weighted entropies by their fraction of the original.
   in (lengthF lefts / lengthF examples) * entropy lefts
    + (lengthF rights / lengthF examples) * entropy rights
 
-informationGain :: Attribute -> Float -> [Example] -> Float
+-- |Information Gain of a group of examples if divided by given attribute and threshold.
+informationGain :: Attribute -> Threshold -> [Example] -> Float
 informationGain attr thresh examples = entropy examples - remainder examples attr thresh
 
+-- |Most common classification in a group of examples.
 pluralityValue :: [Example] -> Clss
 pluralityValue examples = let a_class = exClass $ head examples
                               b_class = head $ filter (/= a_class) $ map exClass examples
@@ -66,36 +84,55 @@ pluralityValue examples = let a_class = exClass $ head examples
                                      | otherwise = b_class
                           in answer
 
+-- |Main Decision Tree learning algorithm.
+-- Given training data, possible attributes to split on, and the most common
+-- class from the parent, it constructs a decision tree that would classify the
+-- given examples correctly.
 learnDecisionTree :: [Example] -> [Attribute] -> Clss -> DecisionTree
 learnDecisionTree examples attrs parent_class
+  -- If no examples remaining, this node becomes a leaf that holds the
+  -- classification passed from the parent.
   | null examples = Leaf parent_class
+  -- If all have the same classification then this is a leaf of that classification.
   | allEqual (map exClass examples) = Leaf (exClass $ head examples)
   -- Note: Attributes never empty - can be reused.
   | otherwise =
-    let calculateGains :: Attribute -> [(Int, Float, Float)]
+    let -- Calculates information gains for all mid-point thresholds for the given attribute.
+        calculateGains :: Attribute -> [(Int, Threshold, Float)]
         calculateGains attr =
-          let vals = sort $ map ((! attr) . exAttrs) examples
+          let -- First get the values for this attribute for each example.
+              vals = sort $ map ((! attr) . exAttrs) examples
+              -- Then sort the values and get the average of each consecutive pair.
               thresholds = map ((1.0/2.0 *) . uncurry (+)) $ zip vals (tail vals)
-          in -- trace ("threshes: " ++ show thresholds ++ "\nvals: " ++ show vals) $
-            map (\t -> (attr, t, informationGain attr t examples)) thresholds
+          -- For each threshold create a tuple of the attribute, the threshold,
+          -- and the information gain that would result by choosing them.
+          in map (\t -> (attr, t, informationGain attr t examples)) thresholds
 
-        -- Reverse attrs in order to prefer earlier attrs for ties for IG (maximumBy takes last).
-        (best_attr, best_thresh, ig) =
+        -- Find the best attribute and threshold to split on by calculating information gain for each
+        -- threshold for each attribute.
+        -- (Reverse attrs in order to prefer earlier attrs for ties for IG  since maximumBy takes last).
+        (best_attr, best_thresh, _) =
             maximumBy (comparing (\(_, _, g) -> g)) $ join $ map calculateGains $ reverse attrs
+        -- Partition the examples by the best attribute and threshold.
         (lefts, rights) = partitionByAttr best_attr best_thresh examples
+        -- Calculate plurality value to pass to children tree nodes.
         this_class = pluralityValue examples
+        -- Helper to recurse on given examples.
         recurse_branch exs = learnDecisionTree exs attrs this_class
-    in -- trace (show best_attr ++ ": " ++ show best_thresh ++ ", ig=" ++ show ig) $
-      Branch best_attr best_thresh (recurse_branch lefts) (recurse_branch rights)
+    -- Construct branch splitting on the attribute and threshold after recursively
+    -- creating left and right subtrees with partitioned examples.
+    in Branch best_attr best_thresh (recurse_branch lefts) (recurse_branch rights)
 
-
-classify :: DecisionTree -> Vector Float -> Example
+-- |Use a decision tree to classify an instance.
+classify :: DecisionTree -> Vector Threshold -> Example
 classify (Leaf clss) attrs = Example attrs clss
 classify (Branch attr thresh left right) attrs
   | attrs ! attr <= thresh = classify left attrs
   | otherwise = classify right attrs
 
 
+-- |The rest of the functions are used to generate a graphviz .dot file that can be
+-- used to render the decision tree.
 nodeName :: Int -> String
 nodeName id' = "n" ++ show id'
 
--- a5/q4_and_5/Main.hs
+++ a5/q4_and_5/Main.hs
@@ -15,16 +15,20 @@ import Data.Function (fix)
 
 import DecisionTree
 
+-- |Removes trailing whitespace and periods.
 trim :: String -> String
 trim = fix (\f s -> if not (null s) && ((||) <$> isSpace <*> (=='.') $ last s) then f (init s) else s)
 
+-- |Parse input string as examples, quitting the program with an error if it fails.
 parseExamples :: String -> IO [Example]
 parseExamples file =
   forM (lines file) $ \line -> do
     let pieces = splitOn "," $ trim line
         attrs = init pieces
+        -- Last number is the label.
         outcome = last pieces
     parsedAttrs <- forM attrs $ \f_string ->
+      -- Parse as float.
       case readMay f_string :: Maybe Float of
         Nothing -> do
           putStrLn $ "Failed to parse " ++ f_string ++ " as a float."
@@ -32,6 +36,7 @@ parseExamples file =
         Just f -> return f
     return $ Example (V.fromList parsedAttrs) outcome
 
+-- |Given a filename and names of attributes, learn a decision tree.
 parseAndLearnTree :: String -> [String] -> IO ([Example], DecisionTree)
 parseAndLearnTree filename attrNames = do
   file <- readFile filename
@@ -39,25 +44,31 @@ parseAndLearnTree filename attrNames = do
   putStrLn $ "Learning on " ++ show (length examples) ++ " training examples..."
   return (examples, learnDecisionTree examples [0..length attrNames - 1] "<none>")
 
+-- |Use a given classifier function to classify a bunch of labeled test data
+-- and return the number of incorrect classifications.
 testClassifier :: (Vector Float -> Example) -> [Example] -> Int
 testClassifier classifier examples =
   let checkExample ex = exClass (classifier $ exAttrs ex) == exClass ex
       wrong_examples = filter (not . checkExample) examples
    in length wrong_examples
 
+-- |Print program usage message and exit.
 usage :: IO ()
 usage = do
   putStrLn "./decision-tree [--dot out.dot] trainData.txt testData.txt feature1 feature2 feature3..."
   exitFailure
 
+-- |Main program.
 main :: IO ()
 main = do
   args <- getArgs
   when (length args < 3) usage
+  -- Get optional name of file to write graphviz output to from argument list.
   let is_dot = head args == "--dot"
       dot_filename = args !! 1
   when (is_dot && length args < 5) usage
 
+  -- Get data filenames and attributes from argument list.
   let args' | is_dot = drop 2 args
             | otherwise = args
       train_filename = head args'
@@ -65,28 +76,31 @@ main = do
       attrNames = drop 2 args'
 
   putStrLn $ "Problem has " ++ show (length attrNames) ++ " attributes."
-
   putStrLn $ "Training on data from " ++ train_filename ++ "."
-  (training_data, tree) <- parseAndLearnTree train_filename attrNames
-  putStrLn $ "Testing on data from " ++ test_filename ++ "."
-  test_file <- readFile test_filename
-  test_data <- parseExamples test_file
 
+  -- Learn on training data.
+  (training_data, tree) <- parseAndLearnTree train_filename attrNames
+
+  -- Output graph.
   when is_dot $ do
     putStrLn $ "Writing dot file " ++ dot_filename ++ "."
     writeFile dot_filename $ decisionTreeToDot tree attrNames
 
+  -- Read training data.
+  putStrLn $ "Testing on data from " ++ test_filename ++ "."
+  test_file <- readFile test_filename
+  test_data <- parseExamples test_file
+
+  -- Use decision tree to classify training data.
   let classifier = classify tree
       num_train_wrong = testClassifier classifier training_data
   putStrLn $ "Correctly classified "
           ++ show (length training_data - num_train_wrong) ++ "/" ++ show (length training_data)
           ++ " training examples!"
 
+  -- Use decision tree to classify test data.
   let num_test_wrong = testClassifier classifier test_data
   putStrLn $ "Correctly classified "
             ++ show (length test_data - num_test_wrong) ++ "/" ++ show (length test_data)
             ++ " test examples."
-
-  --forM_ (zip [0..] attrNames) (\(i, name) -> putStrLn $ name ++ ": " ++ (show $ informationGain i (100.0) training_data))
-
 
